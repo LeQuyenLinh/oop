81854bd44a45b1091ccf4873040a7c9b610ec760
lequyenlinh <24022383@vnu.edu.vn>
2025-02-21 10:12:49 +0700

    add folder

---
+++ oop-main/HW1/Introduction.java
@@ -0,0 +1,27 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Lê Quyền Linh/t" + "24022383/t" +"INT2204 1/t" + "LeQuyenLinh/t" +"24022383@vnu.edu.vn");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("7 bottles of beer on the wall, 7 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("6 bottles of beer on the wall, 6 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("5 bottles of beer on the wall, 5 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("4 bottles of beer on the wall, 4 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("3 bottles of beer on the wall, 3 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("2 bottles of beer on the wall, 2 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+
+
+
+    }
+}
